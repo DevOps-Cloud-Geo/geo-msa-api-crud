d8a0a7ea9e58381b1fef8d7a03a8bedc2c2ef234
Geovanny Bucay <win_geovanny88@hotmail.com>
2025-07-24 10:21:55 -0500

    add cinecction database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'geodb',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
